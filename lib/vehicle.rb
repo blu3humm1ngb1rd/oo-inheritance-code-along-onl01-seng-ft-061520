4d20c712c3948cefa922b68da21700b31d07d74f
BJ Lilley <bethjeanlilley@gmail.com>
2020-07-02 03:43:28 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -4,9 +4,11 @@ class Vehicle
   def initialize(wheel_size,wheel_number)
     @wheel_size = wheel_size
     @wheel_number = wheel_number
-    
   end 
   
+  def go 
+    "vrrrrr"
+  end 
   
   
 end
